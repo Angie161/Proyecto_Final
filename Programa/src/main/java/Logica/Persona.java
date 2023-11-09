aedd84872ae289271d569e4cc32fba03ab0be5a5
Rhussu <wzaratesolar@gmail.com>
2023-11-09 20:01:43 -0300

    Ajustes y documentación en Persona

--- Programa/src/main/java/Logica/Persona.java
+++ Programa/src/main/java/Logica/Persona.java
@@ -1,15 +1,35 @@
 package Logica;
 
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * Esta es una subclase de Almas y representa las almas de las personas.
+ */
 public class Persona extends Almas {
-    public Persona(int bondad, int maldad){
-        super();
-        this.bondad=bondad;
-        this.maldad=maldad;
+
+    /**
+     * Constructor personalizado para poder asignar las características a las personas.
+     *
+     * @param bondad la cantidad de bondad que se le quiere asignar a la persona.
+     * @param maldad  la cantidad de maldad que se le quiere asignar a la persona.
+     * @param color  el color que tendrá la persona.
+     */
+    public Persona(int bondad, int maldad, Color color) {
+        this.bondad = bondad;
+        this.maldad = maldad;
+        this.color  = color;
     }
 
-    @Override
-    public int calcValor(){
-        //Por definir
-        return 0;
+    /**
+     * Implementado calcValor() que contiene la fórmula para calcular el valor de la persona.
+     *
+     * @return el valor de la persona.
+     */
+    public int calcValor() {
+        int    colores    = color.getBlue() + color.getRed() + color.getGreen();
+        double random     = ((new Random()).nextDouble(0.4) + 0.8);
+        double valorFinal = random * Math.pow((maldad * bondad)/(maldad + bondad), 1.5)*(colores + maldad + bondad);
+        return (int) valorFinal;
     }
 }
