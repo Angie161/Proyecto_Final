1c12cff359b0edd878f6c242d859430abce6dd03
Rhussu <wzaratesolar@gmail.com>
2023-11-09 17:44:35 -0300

    Documentación y ajuste de errores en Arcangel

--- Programa/src/main/java/Logica/Arcangel.java
+++ Programa/src/main/java/Logica/Arcangel.java
@@ -2,8 +2,18 @@ package Logica;
 
 import java.awt.*;
 
-public class Arcangel extends Angel{
-    public Arcangel(int bondad, int rango, Color color){
+/**
+ * Subclase de los angeles y es el de menos "poder".
+ */
+public class Arcangel extends Angel {
+
+    /**
+     * Constructor que utiliza el de la superclase.
+     *
+     * @param bondad la bondad del ángel.
+     * @param color el color del ángel.
+     */
+    public Arcangel(int bondad, Color color) {
         super(bondad, 1, color);
     }
 }
