58830782082b2df9cbb156a4941fba69610ec356
Rhussu <wzaratesolar@gmail.com>
2023-11-09 20:14:09 -0300

    Agregada clase Barca

---
+++ Programa/src/main/java/Logica/Barca.java
@@ -0,0 +1,44 @@
+package Logica;
+
+/**
+ * Esta clase representa la barca que usan lso demonios para ir del limbo al mundo terrenal.
+ */
+public class Barca {
+    private boolean funcional;
+
+    /**
+     * Constructor personalizado en el que se inicializan las variables.
+     */
+    public Barca() {
+        funcional = true;
+    }
+
+    /**
+     * Método para trasladar un demonio al mundo terrenal.
+     *
+     * @param mundoTerrenal el mundo terrenal al cual se le quiere trasladar.
+     * @param demonio       el demonio que quiere ser trasladado.
+     * @throws SinCapPermitidaException  la excepción que puede saltar si es que no se permiten más demonios.
+     */
+    public void traslado(MundoTerrenal mundoTerrenal, Demonio demonio) throws SinCapPermitidaException {
+        mundoTerrenal.agregarDemonio(demonio);
+    }
+
+    /**
+     * Getter de la variable funcional.
+     *
+     * @return el valor de la variable.
+     */
+    public boolean getFuncional() {
+        return funcional;
+    }
+
+    /**
+     * Setter de la variable funcional.
+     *
+     * @param funcional  el valor que se le quiere asignar a la variable.
+     */
+    public void setFuncional(boolean funcional) {
+        this.funcional = funcional;
+    }
+}
