deab9f530836b989765689071e16de14b25f9d3a
Rhussu <wzaratesolar@gmail.com>
2023-11-09 18:01:15 -0300

    Eliminadas variables innecesarias y agregada documentación en DepSobre

--- Programa/src/main/java/Logica/DepSobre.java
+++ Programa/src/main/java/Logica/DepSobre.java
@@ -1,26 +1,42 @@
 package Logica;
+
 import java.util.ArrayList;
 
 public class DepSobre<Almas> {
     private ArrayList<Almas> contSobre;
-    private int tam;
 
-    public DepSobre(int tam){
-        contSobre=new ArrayList<Almas>();
-        this.tam=tam;
+    /**
+     * Constructor personalizado en donde se inicializa el ArrayList.
+     */
+    public DepSobre() {
+        contSobre = new ArrayList<Almas>();
     }
 
+    /**
+     * Método para ver la cantidad de almas que hay adentro.
+     *
+     * @return el largo del deposito.
+     */
     public int getTam() {
-        return tam;
+        return contSobre.size();
     }
 
-    public void add(Almas alma){
+    /**
+     * Método para agregar un alma al deposito.
+     *
+     * @param alma el alma que se desea agregar.
+     */
+    public void add(Almas alma) {
         contSobre.add(alma);
-        tam+=1;
     }
 
-    public Almas get(){
-        if(contSobre.size()!=0){
+    /**
+     * Método para sacar la primera alma colocada.
+     *
+     * @return
+     */
+    public Almas get() {
+        if(contSobre.size() != 0) {
             return contSobre.remove(0);
         }
         else{
@@ -30,6 +46,7 @@ public class DepSobre<Almas> {
 
     /**
      * Revisa la última alma almacenada en el depósito sobrenatural.
+     *
      * @return Objeto genérico obtenido del contenedor.
      */
     public Almas see(){
