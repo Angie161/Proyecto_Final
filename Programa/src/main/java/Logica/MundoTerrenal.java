6e5e0483466ab4b7f44a491566f70432ccc3648f
Rhussu <wzaratesolar@gmail.com>
2023-11-09 19:51:48 -0300

    COnfigurados metodos y agregada la documentación en MundoTerrenal

--- Programa/src/main/java/Logica/MundoTerrenal.java
+++ Programa/src/main/java/Logica/MundoTerrenal.java
@@ -1,47 +1,108 @@
 package Logica;
 
-import java.util.ArrayList;
+import java.util.Random;
 
+/**
+ * Esta clase representa el mundo real, donde fallece la gente y donde los demonios son enviados.
+ */
 public class MundoTerrenal {
     private DepSobre demEnviados;
+    private int capacidad;
 
-    public MundoTerrenal(){}
+    /**
+     * Constructor personalizado en el que inicializamos las variables e iniciamos los eventos aleatorios.
+     *
+     * @param barca la barca que se puede destruir.
+     */
+    public MundoTerrenal(Barca barca) {
+        demEnviados = new DepSobre<Demonio>();
+        capacidad   = 1;
+        eventosAleatorios(barca);
+    }
 
-    public void agregarDemonio(Demonio demonio, int capacidad) throws SinCapPermitidaException {
-        if(demEnviados.getTam()<capacidad){
+    /**
+     * Método para que aleatoriamente los demonios sean exorcizados.
+     */
+    private void eventosAleatorios(Barca barca) {
+        Thread tiempo = new Thread(new Runnable() {
+
+            /**
+             * Esto se realiza siempre y es para agregar eventos aleatorios.
+             */
+            @Override
+            public void run() {
+                while(true) {
+                    exorcizar((new Random().nextInt(5000)));
+                    destruccionBarca((new Random().nextInt(5000)), barca);
+                    try {
+                        Thread.sleep(500/demEnviados.getTam());
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                }
+            }
+        });
+        tiempo.start();
+    }
+
+    /**
+     * Método para agregar un demonio al mundo terrenal.
+     *
+     * @param demonio el demonio que se quiere agregar.
+     * @throws SinCapPermitidaException se lanza cuando no se permiten más demonios.
+     */
+    public void agregarDemonio(Demonio demonio) throws SinCapPermitidaException {
+        if(demEnviados.getTam() < capacidad) {
             demEnviados.add(demonio);
-        }
-        else{
+        } else {
             System.out.println("No tienes permitido enviar más demonios");
             throw new SinCapPermitidaException();
         }
     }
 
-    public int muertes(){
-        //Por definir
-        return 0;
+    /**
+     * Setter para la variable capacidad.
+     *
+     * @param capacidad la cantidad que se le desea asignar.
+     */
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
     }
 
-    public void exorcizar(int probExorcismo){
-        //Valor de probabilidad provicional (333 es el num Sagrado)
-        if(probExorcismo==333 && demEnviados.getTam()!=0){
+    /**
+     * La cantidad de gente que muere en el mundo terrenal.
+     *
+     * @return la cantidad de gente que murió.
+     */
+    public int muertes(){
+        int muertes = 1;
+        for(int i = 0; i < demEnviados.getTam(); i++) {
+            muertes += ((Demonio) demEnviados.see(i)).matar();
+        }
+        return muertes;
+    }
+
+    /**
+     * Método que determina si se exorciza o no a un demonio.
+     *
+     * @param probExorcismo el valor aleatorio que se comparará.
+     */
+    private void exorcizar(int probExorcismo) {
+        //Valor de probabilidad provicional (333 es el número Sagrado)
+        if(probExorcismo == 333 && demEnviados.getTam() != 0){
             demEnviados.get();
         }
     }
 
     /**
      * Destruirá la barca si esta se encuentra reparada.
+     *
      * @param probDestruccionBarca entero con un número al azar.
-     * @param funcional booleano con el estado de la barca.
-     * @return booleano destinado a cambiar el estado de la barca.
+     * @param barca la barca que se quiere destruir.
      */
-    public boolean destruccionBarca(int probDestruccionBarca, boolean funcional){
-        //Valor de probabilidad provicional
-        if(probDestruccionBarca==111 && funcional==true){
-            return false;
-        }
-        else {
-            return true;
+    public void destruccionBarca(int probDestruccionBarca, Barca barca) {
+        if(probDestruccionBarca == 111 && barca.getFuncional() == true){
+            barca.setFuncional(false);
         }
     }
 }
