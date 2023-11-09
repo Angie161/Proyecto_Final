aa7ca9282ef5781a36d0692fbd0b71bdd2a80aed
Rhussu <wzaratesolar@gmail.com>
2023-11-09 20:08:28 -0300

    Documentación en Satan

--- Programa/src/main/java/Logica/Satan.java
+++ Programa/src/main/java/Logica/Satan.java
@@ -2,8 +2,18 @@ package Logica;
 
 import java.awt.*;
 
-public class Satan extends Demonio{
-    public Satan(int maldad, Color color){
+/**
+ * Esta es una subclase de Demonio y representa al demonio de mayor nivel.
+ */
+public class Satan extends Demonio {
+
+    /**
+     * Constructor personalizado en el que usamos el constructor de la superclase.
+     *
+     * @param maldad la maldad que le quedemos asignar.
+     * @param color  el color que le queremos asignar.
+     */
+    public Satan(int maldad, Color color) {
         super(maldad,3, color);
     }
 }
