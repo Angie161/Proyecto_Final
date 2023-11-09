17c50ae872b8b15b0eebc20a668fc545fda8b9c3
Rhussu <wzaratesolar@gmail.com>
2023-11-09 17:43:53 -0300

    Agregado algoritmo para calcValor() en Angel y su documentación

--- Programa/src/main/java/Logica/Angel.java
+++ Programa/src/main/java/Logica/Angel.java
@@ -1,20 +1,38 @@
 package Logica;
 
 import java.awt.*;
+import java.util.Random;
 
-public abstract class Angel extends Almas{
+/**
+ * Esta es una subclase de Almas y va a ser superclase de todos los tipos de ángeles.
+ */
+public abstract class Angel extends Almas {
     protected int rango;
 
-    public Angel(int bondad, int rango, Color color){
+    /**
+     * Constructor personalizado para poder asignar las características a los ángeles.
+     *
+     * @param bondad la cantidad de bondad que se le quiere asignar al ángel.
+     * @param rango  el rango que se le asignara al ángel.
+     * @param color  el color que tendrá el ángel.
+     */
+    public Angel(int bondad, int rango, Color color) {
         super();
-        this.bondad=bondad;
-        this.rango=rango;
-        this.color=color;
+        this.bondad = bondad;
+        this.rango  = rango;
+        this.color  = color;
     }
 
+    /**
+     * Override de calcValor() que contiene la fórmula para calcular el valor del ángel.
+     *
+     * @return el valor del ángel.
+     */
     @Override
     public int calcValor() {
-        //Por definir
-        return 0;
+        int    colores    = color.getBlue() + color.getRed() + color.getGreen();
+        double random     = ((new Random()).nextDouble(0.4) + 0.8);
+        double valorFinal = random*Math.pow(rango,1.5)*(colores + bondad);
+        return (int) valorFinal;
     }
 }
