8cd15710c733cc46d0fcb1567adbd189f5d7582e
Rhussu <wzaratesolar@gmail.com>
2023-11-09 17:50:24 -0300

    Agregado algoritmo para calcValor() en Demonio y su documentación

--- Programa/src/main/java/Logica/Demonio.java
+++ Programa/src/main/java/Logica/Demonio.java
@@ -1,22 +1,40 @@
 package Logica;
 
 import java.awt.*;
+import java.util.Random;
 
-public abstract class Demonio extends Almas{
+/**
+ * Esta es una subclase de Almas y va a ser superclase de todos los tipos de demonios.
+ */
+public abstract class Demonio extends Almas {
     protected int rango;
     protected boolean enElLimbo;
 
-    public Demonio(int maldad, int rango, Color color){
+    /**
+     * Constructor personalizado para poder asignar las características a los demonios.
+     *
+     * @param maldad la cantidad de maldad que se le quiere asignar al demonio.
+     * @param rango  el rango que se le asignara al demonio.
+     * @param color  el color que tendrá el demonio.
+     */
+    public Demonio(int maldad, int rango, Color color) {
         super();
-        enElLimbo=true;
-        this.maldad=maldad;
-        this.rango=rango;
-        this.color=color;
+        this.enElLimbo = true;
+        this.maldad    = maldad;
+        this.rango     = rango;
+        this.color     = color;
     }
 
+    /**
+     * Override de calcValor() que contiene la fórmula para calcular el valor del demonio.
+     *
+     * @return el valor del demonio.
+     */
     @Override
     public int calcValor() {
-        //Por definir
-        return 0;
+        int    colores    = color.getBlue() + color.getRed() + color.getGreen();
+        double random     = ((new Random()).nextDouble(0.4) + 0.4);
+        double valorFinal = random * Math.pow(rango,1.5)*(colores + maldad);
+        return (int) valorFinal;
     }
 }
