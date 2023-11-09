4107c6eb22a2742c152c3abcdd701dd01dc29418
Rhussu <wzaratesolar@gmail.com>
2023-11-09 20:04:03 -0300

    Ajustes y documentación en Querubin

--- Programa/src/main/java/Logica/Querubin.java
+++ Programa/src/main/java/Logica/Querubin.java
@@ -2,8 +2,18 @@ package Logica;
 
 import java.awt.*;
 
-public class Querubin extends Angel{
-    public Querubin(int bondad, int rango, Color color) {
-        super(bondad, 3, color);
+/**
+ * Subclase de los angeles y es el de "poder" medio.
+ */
+public class Querubin extends Angel {
+
+    /**
+     * Constructor que utiliza el de la superclase.
+     *
+     * @param bondad la bondad del querubín.
+     * @param color el color del querubín.
+     */
+    public Querubin(int bondad, Color color) {
+        super(bondad, 2, color);
     }
 }
