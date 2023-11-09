f1449cfc5372a193817544577ae1f9f297417a5a
Rhussu <wzaratesolar@gmail.com>
2023-11-09 18:12:57 -0300

    Documentación en Leviathan

--- Programa/src/main/java/Logica/Leviathan.java
+++ Programa/src/main/java/Logica/Leviathan.java
@@ -2,8 +2,18 @@ package Logica;
 
 import java.awt.*;
 
+/**
+ * Esta es una subclase de Demonio y representa al demonio de más bajo nivel.
+ */
 public class Leviathan extends Demonio {
-    public Leviathan(int maldad, Color color){
+
+    /**
+     * Constructor personalizado en el que usamos el constructor de la superclase.
+     *
+     * @param maldad la maldad que le quedemos asignar.
+     * @param color  el color que le queremos asignar.
+     */
+    public Leviathan(int maldad, Color color) {
         super(maldad,1, color);
     }
 }
