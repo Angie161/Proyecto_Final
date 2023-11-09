8ecf2788f856ff7e5b263ca1d77a81ebe8e492ce
Rhussu <wzaratesolar@gmail.com>
2023-11-09 18:10:35 -0300

    Cambio en los modificadores de acceso en los metodos y documentación en LaMuerte

--- Programa/src/main/java/Logica/LaMuerte.java
+++ Programa/src/main/java/Logica/LaMuerte.java
@@ -1,41 +1,68 @@
 package Logica;
 
+/**
+ * Esta clase es el protagonista de nuestro juego y representa al personaje utilizado por el jugador.
+ */
 public class LaMuerte {
     private int fragAlmas;
     private int poder;
     private DepSobre[] depSobre;
 
-    public LaMuerte(){
-        fragAlmas=0;
-        poder=0;
-        depSobre = new DepSobre[3];
+    /**
+     * Constructor personalizado en el que se inicializan las variables.
+     */
+    public LaMuerte() {
+        fragAlmas = 0;
+        poder     = 0;
 
-        depSobre[0] = new DepSobre<Persona>(0);
-        depSobre[0] = new DepSobre<Demonio>(0);
-        depSobre[0] = new DepSobre<Angel>(0);
+        depSobre    = new DepSobre[3];
+        depSobre[0] = new DepSobre<Persona>();
+        depSobre[1] = new DepSobre<Demonio>();
+        depSobre[2] = new DepSobre<Angel>();
     }
 
-    private int getFragAlmas(){
+    /**
+     * Getter de la variable fragAlmas.
+     *
+     * @return la cantidad de fragmentos de alma que tenga el jugador.
+     */
+    public int getFragAlmas() {
         return fragAlmas;
     }
 
-    private void addFragAlmas(int fragAlmasObtenido){
-        fragAlmas+=fragAlmasObtenido;
+    /**
+     * Método para agregar o quitar (colocando un parámetro negativo) fragmentos del alma.
+     *
+     * @param fragAlmasObtenido la cantidad que se quiere agregar o quitar.
+     */
+    public void addFragAlmas(int fragAlmasObtenido) {
+        fragAlmas += fragAlmasObtenido;
     }
 
-    private int getPoder(){
+    /**
+     * Getter de la variable poder.
+     *
+     * @return el poder del personaje.
+     */
+    public int getPoder() {
         return poder;
     }
 
-    private void setPoder(int poderActual){
-        poder=poderActual;
+    /**
+     * Setter para la variable poder.
+     *
+     * @param poderActual el poder que se le desea asignar al jugador.
+     */
+    public void setPoder(int poderActual) {
+        poder = poderActual;
     }
 
     /**
      * Obtiene los depósitos de almas de personas, demonios y angeles.
-     * @return
+     *
+     * @return un arreglo con las referencias a los depósitos que posee la muerte (el jugador).
      */
-    private DepSobre[] getDepSobre(){
+    public DepSobre[] getDepSobre() {
         return depSobre;
     }
 
