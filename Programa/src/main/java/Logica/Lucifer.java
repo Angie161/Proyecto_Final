83c9200474007289c10057e3495ae94814d91f67
Rhussu <wzaratesolar@gmail.com>
2023-11-09 18:14:47 -0300

    Documentación en Lucifer

--- Programa/src/main/java/Logica/Lucifer.java
+++ Programa/src/main/java/Logica/Lucifer.java
@@ -2,8 +2,18 @@ package Logica;
 
 import java.awt.*;
 
-public class Lucifer extends Demonio{
-    public Lucifer(int maldad, Color color){
+/**
+ * Esta es una subclase de Demonio y representa al demonio de nivel medio.
+ */
+public class Lucifer extends Demonio {
+
+    /**
+     * Constructor personalizado en el que usamos el constructor de la superclase.
+     *
+     * @param maldad la maldad que le quedemos asignar.
+     * @param color  el color que le queremos asignar.
+     */
+    public Lucifer(int maldad, Color color) {
         super(maldad,2, color);
     }
 }
