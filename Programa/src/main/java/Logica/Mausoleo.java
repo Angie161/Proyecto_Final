2f8659f3d3a18beb76cc45577aa498de2a9b9ac6
Rhussu <wzaratesolar@gmail.com>
2023-11-09 23:23:47 -0300

    Creada clase Mausoleo

---
+++ Programa/src/main/java/Logica/Mausoleo.java
@@ -0,0 +1,62 @@
+package Logica;
+
+/**
+ * Esta clase representa la "casa" de la muerte y es la que permite hacer mejoras.
+ */
+public class Mausoleo {
+    private Barca barca;
+    private LaMuerte propietario;
+
+    /**
+     * Constructor personalizado en donde inicializamos las variables y asignamos al propietario.
+     *
+     * @param laMuerte el "propietario" del mausoleo.
+     */
+    public Mausoleo(LaMuerte laMuerte) {
+        propietario = laMuerte;
+        barca       = new Barca();
+    }
+
+    /**
+     * Método para enviar un demonio desde el mausoleo.
+     *
+     * @param mundoTerrenal a donde sera enviado.
+     * @param demonio       el que será enviado.
+     * @throws SinCapPermitidaException si es que no se puede enviar el demonio.
+     */
+    public void enviarDemonio(MundoTerrenal mundoTerrenal, Demonio demonio) throws SinCapPermitidaException {
+        barca.traslado(mundoTerrenal, demonio);
+    }
+
+    /**
+     * Método para subir el nivel de la barca lo que permitirá enviar más demonios al mismo tiempo al mundo terrenal.
+     *
+     * @param mundoTerrenal el mundo en el que se le mejorara la capacidad.
+     */
+    public void aumentarCapacidad(MundoTerrenal mundoTerrenal) {
+        mundoTerrenal.setCapacidad(mundoTerrenal.getCapacidad() + 1);
+    }
+
+    /**
+     * Método para aumentar el poder el propietario.
+     */
+    public void aumentarPoder() {
+        propietario.setPoder(propietario.getPoder() + 1);
+    }
+
+    /**
+     * Método para reparar el puente si es q esta roto.
+     *
+     * @param puente el puente que se quiere reparar.
+     */
+    public void repararPuente(Puente puente) {
+        puente.setFuncional(true);
+    }
+
+    /**
+     * Método para reparar la barca del mausoleo si es que está rota.
+     */
+    public void repararBarca() {
+        barca.setFuncional(true);
+    }
+}
