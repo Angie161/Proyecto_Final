4d145a7ad1c5bd881daae02216551222e9e316bb
Angie161 <146099263+Angie161@users.noreply.github.com>
2023-11-13 15:15:34 -0300

    Actualización de clase Precios

--- Programa/src/main/java/Logica/Precios.java
+++ Programa/src/main/java/Logica/Precios.java
@@ -1,52 +1,72 @@
 package Logica;
 
+/**
+ * Clase que contendrá los precios de las mejoras y reparaciones en el Mausoleo.
+ */
 public class Precios {
-    private int precioMejoraCapacidad;
-    private int precioMejoraPoder;
-    private int precioFragEnvio;
-    private int precioAlmasEnvio;
     private LaMuerte laMuerte;
-    private MundoTerrenal mundoTerrenal;
-
-    public Precios(LaMuerte laMuerte, MundoTerrenal mundoTerrenal){
-        precioMejoraCapacidad = 1000;
-        precioMejoraPoder = 1000;
-        precioFragEnvio = 500;
-        precioAlmasEnvio = 5;
-        this.laMuerte=laMuerte;
-        this.mundoTerrenal= mundoTerrenal;
+    
+    /**
+     * Constructor donde se asignan las variables.
+     */
+    public Precios(LaMuerte laMuerte) {
+        this.laMuerte = laMuerte;
     }
 
-    public int getPrecioAlmasEnvio() {
-        return precioAlmasEnvio;
+    /**
+     * Obtendrá la cantidad de ángeles que requerirá el envío de demonios al Mundo Terrenal en base de sus estadisticas actuales. 
+     *
+     * @param mundoTerrenal el mundo terrenal donde se quiera realizar el envío.
+     * @return la cantidad de ángeles que se necesitarán para realizar el siguiente envío.
+     */
+    public int getCantAngelesEnvio(MundoTerrenal mundoTerrenal) {
+        return (5 + mundoTerrenal.getCantDemEnviados() * 5);
     }
 
-    public int getPrecioFragEnvio() {
-        return precioFragEnvio;
+    /**
+     * Obtendrá la cantidad de fragmentos de alma que requerirá el envío de demonios al Mundo Terrenal en base de sus estadisticas actuales.
+     *
+     * @param mundoTerrenal el mundo terrenal donde se quiera realizar el envío.
+     * @return la cantidad de fragmentos que se necesitará para realizar el siguiente envío.
+     */
+    public int getPrecioFragEnvio(MundoTerrenal mundoTerrenal) {
+        return mundoTerrenal.getCantDemEnviados() * laMuerte.getPoder() * 100 + 500 * laMuerte.getPoder();
     }
 
-    public int getPrecioMejoraCapacidad() {
-        return precioMejoraCapacidad;
+    /**
+     * Obtendrá el precio que requerirá cada mejora de la capacidad del Mundo Terrenal.
+     *
+     * @param mundoTerrenal el mundo terrenal al que se le quiere aumentar la capacidad.
+     * @return el precio necesario para aumentar la capacidad del Mundo Terrenal.
+     */
+    public int getPrecioMejoraCapacidad(MundoTerrenal mundoTerrenal) {
+        return mundoTerrenal.getCapacidad() * 1500;
     }
 
+    /**
+     * Obtendrá el precio que se requerirá para aumentar el poder de LaMuerte.
+     *
+     * @return el precio necesario para aumentar el poder de LaMuerte.
+     */
     public int getPrecioMejoraPoder() {
-        return precioMejoraPoder;
+        return Math.pow(laMuerte.getPoder(), 2) * 2000;
+    }
+    
+    /**
+     * Obtendrá el precio que se requerirá para reparar la barca.
+     *
+     * @return el precio necesario para reparar la barca.
+     */
+    public int getPrecioRepararBarca() {
+        return laMuerte.getPoder() * 5000;
     }
 
-    public void actualPrecioCapacidad(int tiempoTranscurrido){
-        try {
-            precioMejoraCapacidad = (int) (mundoTerrenal.getCapacidad() * 1000 + (tiempoTranscurrido * 2) * Math.pow(laMuerte.getPoder(), 2));
-        }catch (NullPointerException e){
-            return;
-        }
-    }
-
-    public void actualPrecioPoder(int tiempoTranscurrido){
-        precioMejoraPoder = (int)((tiempoTranscurrido*2) * laMuerte.getPoder() * 1000 * Math.pow(mundoTerrenal.getCapacidad(),2));
-    }
-
-    public void actualPrecioEnvio(int tiempoTranscurrido){
-        precioFragEnvio = (int)((tiempoTranscurrido*2) * mundoTerrenal.getCantDemEnviados()*1000);
-        precioAlmasEnvio=(int)((tiempoTranscurrido) + mundoTerrenal.getCantDemEnviados()*10);
+    /**
+     * Obtendrá el precio que se requerirá para reparar el puente.
+     *
+     * @return el precio necesario para reparar el puente.
+     */
+    public int getPrecioRepararaPuente() {
+        return laMuerte.getPoder() * 10000;
     }
 }
