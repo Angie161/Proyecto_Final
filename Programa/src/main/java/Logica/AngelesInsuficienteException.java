b0cbb2499634b9e3773c5ba63dbc764e1af1c434
Rhussu <wzaratesolar@gmail.com>
2023-11-13 21:20:09 -0300

    Nuevas excepciones

---
+++ Programa/src/main/java/Logica/AngelesInsuficienteException.java
@@ -0,0 +1,14 @@
+package Logica;
+
+/**
+ * Excepción para representar cuando no hay ángeles suficientes para enviar un demonio.
+ */
+public class AngelesInsuficienteException extends Exception{
+
+    /**
+     * Constructor por defecto.
+     */
+    public AngelesInsuficienteException() {
+        super();
+    }
+}
---
+++ Programa/src/main/java/Logica/FragmentosInsuficientesException.java
@@ -0,0 +1,14 @@
+package Logica;
+
+/**
+ * Excepción para representar cuando no hay fragmentos de almas suficientes para comprar algo.
+ */
+public class FragmentosInsuficientesException extends Exception {
+
+    /**
+     * Constructor por defecto.
+     */
+    public FragmentosInsuficientesException() {
+        super();
+    }
+}
