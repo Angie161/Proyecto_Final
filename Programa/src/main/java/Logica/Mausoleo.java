5b5a0bef0b339fb8944c5671b99f2cfdb4f9cc8d
Rhussu <wzaratesolar@gmail.com>
2023-11-13 21:38:46 -0300

    Ajuste de los métodos para comprar/reparar cosas en Mausoleo

--- Programa/src/main/java/Logica/Mausoleo.java
+++ Programa/src/main/java/Logica/Mausoleo.java
@@ -6,48 +6,63 @@ package Logica;
 public class Mausoleo {
     private Barca barca;
     private LaMuerte propietario;
+    private MundoTerrenal tierra;
+    private Precios precios;
 
     /**
      * Constructor personalizado en donde inicializamos las variables y asignamos al propietario.
      *
      * @param laMuerte el "propietario" del mausoleo.
      */
-    public Mausoleo(LaMuerte laMuerte) {
+    public Mausoleo(LaMuerte laMuerte, MundoTerrenal mundoTerrenal) {
         propietario = laMuerte;
+        tierra      = mundoTerrenal;
         barca       = new Barca();
+        precios     = new Precios(propietario);
     }
 
     /**
      * Método para enviar un demonio desde el mausoleo.
      *
-     * @param mundoTerrenal a donde sera enviado.
-     * @param demonio       el que será enviado.
-     * @throws SinCapPermitidaException si es que no se puede enviar el demonio.
+     * @throws SinCapPermitidaException Cuando no se pueden enviar más demonios porque ya se envió el máximo.
+     * @throws BarcaRotaException Cuando la barca está rota y se intenta manda un demonio.
+     * @throws AngelesInsuficienteException cuando no hay suficientes angeles en los depósitos para enviar el demonio.
+     * @throws FragmentosInsuficientesException cuando no hay suficientes fragmentos de almas para enviar el demonio.
      */
-    public void enviarDemonio(MundoTerrenal mundoTerrenal, Demonio demonio) throws SinCapPermitidaException, BarcaRotaException {
-        try {
-            barca.traslado(mundoTerrenal, demonio);
-        }catch (BarcaRotaException b){
-            throw b;
-        }catch (SinCapPermitidaException s){
-            throw s;
+    public void enviarDemonio() throws SinCapPermitidaException, BarcaRotaException, AngelesInsuficienteException, FragmentosInsuficientesException {
+        if(propietario.getDepSobre()[2].getTam() >= precios.getCantAngelesEnvio(tierra) && propietario.getFragAlmas() >= precios.getPrecioFragEnvio(tierra)) {
+            enviarDemonio();
+            for(int i = 0; i < precios.getCantAngelesEnvio(tierra); i++) {
+                barca.traslado(tierra, (Demonio) propietario.getDepSobre()[1].get());
+            }
+            propietario.addFragAlmas(-precios.getPrecioFragEnvio(tierra));
+        } else if(propietario.getDepSobre()[2].getTam() < precios.getCantAngelesEnvio(tierra)) {
+            throw new AngelesInsuficienteException();
+        } else if(propietario.getFragAlmas() < precios.getPrecioFragEnvio(tierra)) {
+            throw new FragmentosInsuficientesException();
         }
     }
 
     /**
      * Método para subir el nivel de la barca lo que permitirá enviar más demonios al mismo tiempo al mundo terrenal.
-     *
-     * @param mundoTerrenal el mundo en el que se le mejorara la capacidad.
      */
-    public void aumentarCapacidad(MundoTerrenal mundoTerrenal) {
-        mundoTerrenal.setCapacidad(mundoTerrenal.getCapacidad() + 1);
+    public void aumentarCapacidad() throws FragmentosInsuficientesException {
+        if(propietario.getFragAlmas() >= precios.getPrecioMejoraCapacidad(tierra) && propietario.getPoder() * 2 >= tierra.getCapacidad()) {
+            tierra.setCapacidad(tierra.getCapacidad() + 1);
+        } else {
+            throw new FragmentosInsuficientesException();
+        }
     }
 
     /**
      * Método para aumentar el poder el propietario.
      */
-    public void aumentarPoder() {
-        propietario.setPoder(propietario.getPoder() + 1);
+    public void aumentarPoder() throws FragmentosInsuficientesException {
+        if(propietario.getFragAlmas() >= precios.getPrecioMejoraPoder()) {
+            propietario.setPoder(propietario.getPoder() + 1);
+        } else {
+            throw new FragmentosInsuficientesException();
+        }
     }
 
     /**
@@ -55,14 +70,22 @@ public class Mausoleo {
      *
      * @param puente el puente que se quiere reparar.
      */
-    public void repararPuente(Puente puente) {
-        puente.setFuncional(true);
+    public void repararPuente(Puente puente) throws FragmentosInsuficientesException {
+        if(propietario.getFragAlmas() >= precios.getPrecioRepararaPuente() && !puente.getFuncional()) {
+            puente.setFuncional(true);
+        } else {
+            throw new FragmentosInsuficientesException();
+        }
     }
 
     /**
      * Método para reparar la barca del mausoleo si es que está rota.
      */
-    public void repararBarca() {
-        barca.setFuncional(true);
+    public void repararBarca() throws FragmentosInsuficientesException {
+        if(propietario.getFragAlmas() >= precios.getPrecioRepararaPuente() && !barca.getFuncional()) {
+            barca.setFuncional(true);
+        } else {
+            throw new FragmentosInsuficientesException();
+        }
     }
 }
