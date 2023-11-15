824dca27274fc8a0508fa55eeed7150fe9cf13ac
Rhussu <wzaratesolar@gmail.com>
2023-11-15 19:13:45 -0300

    Añadida variable Almas a clase PanelAlma

--- Programa/src/main/java/Interfaz/PanelAlma.java
+++ Programa/src/main/java/Interfaz/PanelAlma.java
@@ -1,14 +1,20 @@
 package Interfaz;
 
 import GameTools.Hitbox;
+import Logica.Almas;
 
 import javax.swing.*;
 
 public class PanelAlma extends JPanel {
+    private final Almas alma = null;
     private Hitbox hitbox;
     public PanelAlma() {
         super();
         setBounds(200 + Hitbox.getTodasLasHitbox().size()*70 ,350,60,100);
         hitbox = new Hitbox(getBounds(), this);
+    }
+
+    public Almas getAlma() {
+        return alma;
     }
 }
