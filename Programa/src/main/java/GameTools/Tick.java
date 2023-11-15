54589204325453e2d638d4573cd2a78746754148
Rhussu <wzaratesolar@gmail.com>
2023-11-15 14:56:00 -0300

    Agregada verificación de hitbox v1.00 en Tick

--- Programa/src/main/java/GameTools/Tick.java
+++ Programa/src/main/java/GameTools/Tick.java
@@ -1,7 +1,10 @@
 package GameTools;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Tick extends Thread {
-    private static final int tick = 10;
+    private static final int tick = 5;
     private Controles controles;
     public Tick(Controles controles){
         super();
@@ -13,6 +16,12 @@ public class Tick extends Thread {
     public void run() {
         while(true) {
             jugadorSigueAControles();
+            ArrayList<Hitbox> hitboxes = Hitbox.getTodasLasHitbox();
+            for(int i = 1; i < hitboxes.size(); i++) {
+                if(hitboxes.get(0).getHitbox().intersects(hitboxes.get(i).getHitbox())) {
+                    moverHitbox(hitboxes.get(i));
+                }
+            }
             try {
                 Thread.sleep(Tick.tick);
             } catch (InterruptedException e) {
@@ -20,10 +29,18 @@ public class Tick extends Thread {
             }
         }
     }
+
+    private void moverHitbox(Hitbox hitbox) {
+        hitbox.setVelocidad(Hitbox.getTodasLasHitbox().get(0).getVelocidad());
+        hitbox.setLocation(hitbox.getLocation().x + hitbox.getVelocidad().x, hitbox.getLocation().y + hitbox.getVelocidad().y);
+        hitbox.setVelocidad(new Point(0, 0));
+        hitbox.getPanelAsociado().setLocation(hitbox.getLocation());
+    }
+
     private void jugadorSigueAControles() {
         controles.setLocation(controles.getLocation().x + controles.getJugador().getVelocidad().x, controles.getLocation().y + controles.getJugador().getVelocidad().y);
         controles.getJugador().setLocation(controles.getLocation());
-        //controles.getJugador().getHitbox().setLocation(controles.getLocation());
-        //controles.getJugador().getHitbox().setVelocidad(controles.getJugador().getVelocidad());
+        controles.getJugador().getHitbox().setLocation(controles.getLocation());
+        controles.getJugador().getHitbox().setVelocidad(controles.getJugador().getVelocidad());
     }
 }
