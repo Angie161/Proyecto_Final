5a7d756bfc85cc6b93c81e638541220e9a4c97e1
Angie161 <aramirez2022@udec.cl>
2023-11-15 22:14:05 -0300

    Agregada la documentación de la clase DemoniosFactory y sus subclases

--- Programa/src/main/java/Factories/DemoniosFactory.java
+++ Programa/src/main/java/Factories/DemoniosFactory.java
@@ -4,14 +4,22 @@ import Logica.*;
 import java.awt.*;
 import java.util.Random;
 
+/**
+ * Clase que representa y funciona como una fábrica de almas de Demonios, la cual creará un subtipo de demonio dependiendo de un valor al azar.
+ */
 public class DemoniosFactory extends AlmasFactory{
     protected int numSelect;
     private int maldad;
-
     private LuciferFactory luciferFactory = new LuciferFactory();
     private SatanFactory satanFactory = new SatanFactory();
     private LeviathanFactory leviathanFactory = new LeviathanFactory();
 
+    /**
+     * Genera almas de demonios a partir de un número random que puede ser 1, 2 o 3 para obtener un Leviathan, lucifer o satan.
+     * @param laMuerte con la cual obtendremos sus estadísticas de poder para influir en el cálculo de la maldad de los demonios.
+     * @param color que se pasará como parámetro al método de creación de un alma de demonio específico.
+     * @return demonio de un tipo al azar.
+     */
     public Demonio crearDemonio(LaMuerte laMuerte, Color color){
         Random random = new Random();
 
--- Programa/src/main/java/Factories/SatanFactory.java
+++ Programa/src/main/java/Factories/SatanFactory.java
@@ -3,7 +3,16 @@ package Factories;
 import Logica.*;
 import java.awt.*;
 
+/**
+ * Clase que representa y funciona como una fábrica de demonios de tipo Satan.
+ */
 public class SatanFactory extends DemoniosFactory{
+    /**
+     * Crea un demonio del tipo Satan.
+     * @param maldad la cual se le asignará al nuevo demonio como estadística.
+     * @param color el cual se le asignará al nuevo demonio como estadística.
+     * @return demonio Satan con las características concedidas.
+     */
     public Demonio crearSatan(int maldad, Color color){
         return new Leviathan(maldad,color);
     }
