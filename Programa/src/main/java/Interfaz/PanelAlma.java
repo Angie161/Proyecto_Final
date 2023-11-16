8278b43519e084ce302a7be97e089fdea6c95a01
Rhussu <wzaratesolar@gmail.com>
2023-11-16 14:54:45 -0300

    Ahora PanelAlma muestra el color del Alma y puede ser purificado

--- Programa/src/main/java/Interfaz/PanelAlma.java
+++ Programa/src/main/java/Interfaz/PanelAlma.java
@@ -4,14 +4,16 @@ import GameTools.Hitbox;
 import Logica.Almas;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelAlma extends JPanel {
-    private final Almas alma = null;
+    private final Almas alma;
     private Hitbox hitbox;
-    public PanelAlma() {
+    public PanelAlma(Almas alma) {
         super();
         setSize(60,100);
         hitbox = new Hitbox(getBounds(), this);
+        this.alma = alma;
     }
 
     public Hitbox getHitbox() {
@@ -20,5 +22,16 @@ public class PanelAlma extends JPanel {
 
     public Almas getAlma() {
         return alma;
+    }
+    public PanelAlma purificar() {
+        Hitbox.eliminarHitbox(hitbox);
+        return this;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(alma.getColor());
+        g.fillOval(40,80,15,15);
     }
 }
