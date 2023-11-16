cb41b444bae5e346f74d140c8db8c2ebe24d98ad
Angie161 <aramirez2022@udec.cl>
2023-11-15 21:40:34 -0300

    Añadido el método de creación de Personas a clase PersonaFactory

--- Programa/src/main/java/Factories/PersonaFactory.java
+++ Programa/src/main/java/Factories/PersonaFactory.java
@@ -1,10 +1,19 @@
 package Factories;
 
+import Logica.*;
 import java.awt.*;
+import java.util.Random;
 
 public class PersonaFactory extends AlmasFactory{
-    @Override
-    public Color selectColor() {
-        return null;
+    protected int maldadRandom;
+    protected int bondadRandom;
+
+    public Persona crearPersona(LaMuerte laMuerte, Color color){
+        Random random = new Random();
+
+        maldadRandom=(int)(random.nextInt(100) * Math.pow(laMuerte.getPoder(),2));
+        bondadRandom=(int)(random.nextInt(100) * Math.pow(laMuerte.getPoder(),2));
+
+        return new Persona(bondadRandom,maldadRandom,color);
     }
 }
