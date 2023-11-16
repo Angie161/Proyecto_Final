55b0b5e37501fcd48561a42cdb0e1e2dc4465726
Angie161 <aramirez2022@udec.cl>
2023-11-15 21:59:53 -0300

    Añadida la documentación de clase AlmasFactory

--- Programa/src/main/java/Factories/AlmasFactory.java
+++ Programa/src/main/java/Factories/AlmasFactory.java
@@ -4,6 +4,9 @@ import Logica.*;
 import java.awt.*;
 import java.util.Random;
 
+/**
+ * Clase que representa y funciona como una fábrica de almas, permitiendo crear de manera al azar un tipo de alma, ya se Angeles, Personas o Demonios.
+ */
 public abstract class AlmasFactory{
     protected int numSelect;
     private int numColor;
@@ -11,6 +14,11 @@ public abstract class AlmasFactory{
     private PersonaFactory personaFactory = new PersonaFactory();
     private DemoniosFactory demoniosFactory = new DemoniosFactory();
 
+    /**
+     * Crea un tipo de alma a partir de un número random.
+     * @param laMuerte la cual se solicita para posteriormente acceder a sus estadísticas.
+     * @return un alma de un tipo al azar.
+     */
     public Almas crearAlmas(LaMuerte laMuerte){
         Random random = new Random();
         numSelect=random.nextInt(101);
@@ -24,6 +32,11 @@ public abstract class AlmasFactory{
         }
     }
 
+    /**
+     * Obtiene un color al azar dentro de la gama de colores permitida para cada tipo de alma.
+     * @param cual entero con un valor entre 0 y 2 que permitirá seleccionar la gama de colores.
+     * @return un color al azar dentro de la gama correspondiente al alma.
+     */
     private Color getColorAleatorio(int cual) {
         Random random = new Random();
         // 0 para persona, 1 para demonios y 2 para ángeles.
@@ -33,11 +46,13 @@ public abstract class AlmasFactory{
             int green = random.nextInt(230) + 25;
             int blue  = random.nextInt(230) + 25;
             return new Color(red, green, blue);
+
         } else if(cual == 1) {
             // Color para los demonios.
             Double r = random.nextDouble(100);
             int red = (int) (-Math.pow(r, 3)*0.00049 + Math.pow(r, 2)*0.0685 + 24.9);
             return new Color(red, 0, 0);
+
         } else if(cual == 2) {
             // Color para los ángeles.
             Double r = random.nextDouble(100);
@@ -45,6 +60,7 @@ public abstract class AlmasFactory{
             int green = (int) (-Math.pow(r, 3)*0.000024897959 - Math.pow(r, 2)*0.00091 + 255);
             int blue  = (int) (-Math.pow(r, 3)*0.0001534 - Math.pow(r, 2)*0.00415986 + 255);
             return new Color(red, green, blue);
+
         } else {
             // Si se ingresa un numero erroneo.
             return null;
