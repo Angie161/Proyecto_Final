f71988cb570329b861a94dea4c1fd4d9b1605a7a
Angie161 <aramirez2022@udec.cl>
2023-11-15 22:19:48 -0300

    Agregada la documentación de la clase AngelesFactory y sus subclases

--- Programa/src/main/java/Factories/AngelesFactory.java
+++ Programa/src/main/java/Factories/AngelesFactory.java
@@ -4,14 +4,22 @@ import Logica.*;
 import java.awt.*;
 import java.util.Random;
 
+/**
+ * Clase que representa y funciona como una fábrica de almas de Angeles, la cual creará un subtipo de ángel dependiendo de un valor al azar.
+ */
 public class AngelesFactory extends AlmasFactory{
     protected int numSelect;
     private int bondad;
-
     private ArcangelFactory arcangelFactory = new ArcangelFactory();
     private QuerubinFactory querubinFactory = new QuerubinFactory();
     private SerafineFactory serafineFactory = new SerafineFactory();
 
+    /**
+     * Genera almas de ángeles a partir de un número random que puede ser 1, 2 o 3 para obtener un arcángel, querubín o serafine.
+     * @param laMuerte con la cual obtendremos sus estadísticas de poder para influir en el cálculo de la bondad de los ángeles.
+     * @param color que se pasará como parámetro al método de creación de un alma de ángel específico.
+     * @return ángel de un tipo al azar.
+     */
     public Angel crearAngel(LaMuerte laMuerte, Color color){
         Random random = new Random();
 
--- Programa/src/main/java/Factories/QuerubinFactory.java
+++ Programa/src/main/java/Factories/QuerubinFactory.java
@@ -3,8 +3,16 @@ package Factories;
 import Logica.*;
 import java.awt.*;
 
+/**
+ * Clase que representa y funciona como una fábrica de ángeles de tipo Querubín.
+ */
 public class QuerubinFactory extends AngelesFactory{
-
+    /**
+     * Crea un ángel Querubín.
+     * @param bondad la cual se le asignará al nuevo ángel como estadística.
+     * @param color el cual se le asignará al nuevo ángel como estadística.
+     * @return Querubín con las características concedidas.
+     */
     public Angel crearQuerubin(int bondad, Color color){
         return new Querubin(bondad,color);
     }
