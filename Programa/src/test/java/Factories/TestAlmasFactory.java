e5ec494b7951ed1845a63efa39355c3d2584da83
Angie161 <aramirez2022@udec.cl>
2023-11-19 01:02:47 -0300

    Añadidos nuevos Tests Unitarios con documentación

--- Programa/src/test/java/Factories/TestAlmasFactory.java
+++ Programa/src/test/java/Factories/TestAlmasFactory.java
@@ -1,7 +1,51 @@
 package Factories;
 
+import Logica.LaMuerte;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Clase del tipo test unitario para probar el correcto funcionamiento de los métodos de creación de almas de las Factory.
+ */
 class TestAlmasFactory {
+    private AlmasFactory almasFactory;
+    private AngelesFactory angelesFactory;
+    private DemoniosFactory demoniosFactory;
+    private PersonaFactory personaFactory;
+    private LaMuerte laMuerte;
+    private Color color;
+    int i=100;
 
+    /**
+     * Inicializa las variables que se emplearán en todas las pruebas.
+     */
+    @BeforeEach
+    void setup(){
+        almasFactory = new AlmasFactory();
+        angelesFactory = new AngelesFactory();
+        demoniosFactory = new DemoniosFactory();
+        personaFactory = new PersonaFactory();
+        laMuerte = new LaMuerte();
+        color = new Color(255,255,255);
+    }
+
+    /**
+     * Se realizan 100 creaciones de almas distintas y se verifica que no sean nulas.
+     * Se ejecuta esta cantidad de veces porque para crear almas se seleccionan números al azar entre el 1 y el 3 para elegir que subtipo de alma se crea,
+     * por ende se realizan varias repeticiones para asegurar que no haya algún caso default.
+     */
+    @Test
+    @DisplayName("Test de creación de almas")
+    void creacionAlmas(){
+        while(i!=0){
+            assertNotNull(almasFactory.crearAlmas(laMuerte));
+            assertNotNull(angelesFactory.crearAngel(laMuerte, color));
+            assertNotNull(demoniosFactory.crearDemonio(laMuerte, color));
+            assertNotNull(personaFactory.crearPersona(laMuerte, color));
+            i-=1;
+        }
+    }
 }
--- Programa/src/test/java/Logica/TestMundoTerrenal.java
+++ Programa/src/test/java/Logica/TestMundoTerrenal.java
@@ -1,4 +1,75 @@
 package Logica;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.awt.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Clase del tipo test unitario para probar el correcto funcionamiento del método de agregarDemonio() en clase MundoTerrenal.
+ */
 public class TestMundoTerrenal {
+    private MundoTerrenal mundoTerrenal;
+    private Barca barca;
+    private Color color;
+    private Lucifer lucifer;
+    private Satan satan;
+
+    /**
+     * Inicializa las variables que se emplearán en todas las pruebas.
+     */
+    @BeforeEach
+    void setup(){
+        barca = new Barca();
+        mundoTerrenal = new MundoTerrenal(barca);
+        color = new Color(255,0,0);
+        lucifer = new Lucifer(30, color);
+        satan = new Satan(50, color);
+    }
+
+    /**
+     * Se agrega un demonio en el depósito en mundo terrenal y se corroboran los estados iniciales y finales.
+     *
+     * @throws SinCapPermitidaException En caso de que se intente agregar otro demonio cuando no haya más capacidad en el mundo terrenal.
+     */
+    @Test
+    @DisplayName("Test añadido de demonio al mundo terrenal")
+    void agregadoDemonios() throws SinCapPermitidaException {
+        assertEquals(0, mundoTerrenal.getCantDemEnviados());
+        mundoTerrenal.agregarDemonio(lucifer);
+        assertEquals(1, mundoTerrenal.getCantDemEnviados());
+    }
+
+    /**
+     * Se prueba agregar dos demonios cuando solo hay capacidad para uno.
+     * Se busca corroborar el correcto funcionamiento de la excepción SinCapPermitidaException.
+     *
+     * @throws SinCapPermitidaException En caso de que se intente agregar otro demonio cuando no haya más capacidad en el mundo terrenal.
+     */
+    @Test
+    @DisplayName("Test sin capacidad")
+    void sinCapacidad() throws SinCapPermitidaException {
+        assertEquals(1, mundoTerrenal.getCapacidad());
+        assertEquals(0, mundoTerrenal.getCantDemEnviados());
+        mundoTerrenal.agregarDemonio(lucifer);
+        assertEquals(1, mundoTerrenal.getCantDemEnviados());
+        assertThrows(SinCapPermitidaException.class, () ->{
+            mundoTerrenal.agregarDemonio(satan);
+        });
+    }
+
+    /**
+     * Se prueba que la cantidad de muertes generadas por un solo demonio en el mundo terrenal sea la correcta.
+     *
+     * @throws SinCapPermitidaException En caso de que se intente agregar otro demonio cuando no haya más capacidad en el mundo terrenal.
+     */
+    @Test
+    @DisplayName("Test muertes por un demonio")
+    void revisionMuertes() throws SinCapPermitidaException {
+        mundoTerrenal.agregarDemonio(lucifer);
+        assertEquals(1, mundoTerrenal.getCantDemEnviados());
+
+        assertEquals(85, mundoTerrenal.muertes());
+    }
 }
