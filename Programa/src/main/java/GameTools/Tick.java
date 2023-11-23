ff7a1be94ca31eb79adeb80bf4fba4f9489c3eda
Rhussu <wzaratesolar@gmail.com>
2023-11-23 03:17:17 -0300

    Versión 1.5 de las Hitboxs y guardado de las almas en los depSobre

--- Programa/src/main/java/GameTools/Tick.java
+++ Programa/src/main/java/GameTools/Tick.java
@@ -1,10 +1,11 @@
 package GameTools;
 
-import Interfaz.PanelAlma;
-import Interfaz.PanelLaMuerte;
-import Interfaz.PanelMapa;
-import Interfaz.Ventana;
+import Interfaz.*;
+import Logica.Angel;
+import Logica.Demonio;
+import Logica.Persona;
 
+import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
@@ -24,6 +25,7 @@ public class Tick extends Thread {
     public void run() {
         while(true) {
             ArrayList<Hitbox> hitboxes = Hitbox.getTodasLasHitbox();
+            //entrarAlMenu(hitboxes.get(0));
             for(int i = 1; i < hitboxes.size(); i++) {
                 if(hitboxes.get(0).getHitbox().intersects(hitboxes.get(i).getHitbox())) {
                     moverHitbox(hitboxes.get(i));
@@ -42,10 +44,11 @@ public class Tick extends Thread {
     }
 
     private void moverHitbox(Hitbox hitbox) {
-        if(hitbox.isMovible()) {
+        if (hitbox.isMovible()) {
             hitbox.setVelocidad(Hitbox.getTodasLasHitbox().get(0).getVelocidad());
             hitbox.setLocation(hitbox.getLocation().x + hitbox.getVelocidad().x, hitbox.getLocation().y + hitbox.getVelocidad().y);
             quemarAlma(hitbox);
+            guardarAlma(hitbox);
             for (int i = 1; i < Hitbox.getTodasLasHitbox().size(); i++) {
                 if (Hitbox.getTodasLasHitbox().get(i) != hitbox && Hitbox.getTodasLasHitbox().get(i).getHitbox().intersects(hitbox.getHitbox())) {
                     moverHitbox(Hitbox.getTodasLasHitbox().get(i));
@@ -56,13 +59,12 @@ public class Tick extends Thread {
             if (!hitbox.isTraspasableForHitbox() && !hitbox.isTraspasableForPlayer()) {
                 for (int i = Hitbox.NoMovibleHitboxs(); i < Hitbox.getTodasLasHitbox().size(); i++) {
                     Hitbox.getTodasLasHitbox().get(i).setLocation(Hitbox.getTodasLasHitbox().get(i).getLocation().x - Hitbox.getTodasLasHitbox().get(i).getVelocidad().x, Hitbox.getTodasLasHitbox().get(i).getLocation().y - Hitbox.getTodasLasHitbox().get(i).getVelocidad().y);
-                    Hitbox.getTodasLasHitbox().get(i).setVelocidad(0, 0);
                 }
                 controles.setLocation(controles.getLocation().x - controles.getJugador().getVelocidad().x, controles.getLocation().y - controles.getJugador().getVelocidad().y);
-                controles.getJugador().setVelocidad(new Point(0,0));
+                controles.getJugador().setVelocidad(new Point(0, 0));
             } else if (hitbox.isTraspasableForHitbox() && !hitbox.isTraspasableForPlayer() && hitbox.getHitbox().intersects(Hitbox.getTodasLasHitbox().get(0).getHitbox())) {
                 controles.setLocation(controles.getLocation().x - controles.getJugador().getVelocidad().x, controles.getLocation().y - controles.getJugador().getVelocidad().y);
-                controles.getJugador().setVelocidad(new Point(0,0));
+                controles.getJugador().setVelocidad(new Point(0, 0));
             }
         }
             /*
@@ -105,6 +107,30 @@ public class Tick extends Thread {
             panelMapa.remove(((PanelAlma) h.getPanelAsociado()).purificar());
             Spawner.unAlmaMenos();
             System.out.println("Alma quemada");
+        }
+    }
+    private void guardarAlma(Hitbox h) {
+        if(((PanelAlma) h.getPanelAsociado()).getAlma() instanceof Angel && (h.getLocation().x >= 75 && h.getLocation().x <= 137 - h.getHitbox().width) && (h.getLocation().y > 280 && h.getLocation().y < (400 - h.getHitbox().height))) {
+            panelMapa.getPanelDepSobres()[0].getDepSobre().add(((PanelAlma) h.getPanelAsociado()).getAlma());
+            panelMapa.remove(h.getPanelAsociado());
+            Hitbox.eliminarHitbox(h);
+        } else if(((PanelAlma) h.getPanelAsociado()).getAlma() instanceof Demonio && (h.getLocation().x >= 75 && h.getLocation().x <= 137 - h.getHitbox().width) && (h.getLocation().y > 420 && h.getLocation().y < (540 - h.getHitbox().height))) {
+            panelMapa.getPanelDepSobres()[2].getDepSobre().add(((PanelAlma) h.getPanelAsociado()).getAlma());
+            panelMapa.remove(h.getPanelAsociado());
+            Hitbox.eliminarHitbox(h);
+        } else if(((PanelAlma) h.getPanelAsociado()).getAlma() instanceof Persona && (h.getLocation().x >= 75 && h.getLocation().x <= 137 - h.getHitbox().width) && (h.getLocation().y > 560 && h.getLocation().y < (680 - h.getHitbox().height))) {
+            panelMapa.getPanelDepSobres()[1].getDepSobre().add(((PanelAlma) h.getPanelAsociado()).getAlma());
+            panelMapa.remove(h.getPanelAsociado());
+            Hitbox.eliminarHitbox(h);
+        }
+    }
+    private void entrarAlMenu(Hitbox h) {
+        if((h.getLocation().x >= 300 && h.getLocation().x <= 650 ) && (h.getLocation().y > 100 && h.getLocation().y < 150)) {
+            PanelMenuMausoleo pm = new PanelMenuMausoleo(panelMapa);
+            panelMapa.add(pm);
+            panelMapa.setComponentZOrder(pm, 0);
+            controles.setLocation(controles.getLocation().x - controles.getJugador().getVelocidad().x, controles.getLocation().y - controles.getJugador().getVelocidad().y);
+            controles.getJugador().setVelocidad(new Point(0, 0));
         }
     }
 }
