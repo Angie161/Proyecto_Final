ec2366014460e5f1ace95dcbd889a4bb6b683306
Rhussu <wzaratesolar@gmail.com>
2023-11-23 23:18:14 -0300

    Nueva clase PanelSalidaDepSobre que representa donde sacamos las almas

---
+++ Programa/src/main/java/Interfaz/PanelSalidaDepSobre.java
@@ -0,0 +1,30 @@
+package Interfaz;
+
+import GameTools.Hitbox;
+import Logica.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelSalidaDepSobre extends JPanel {
+    private static Dimension size = new Dimension(120, 100);
+    private Point ubicacion;
+    private DepSobre depSobre;
+    public PanelSalidaDepSobre(int x, int y, DepSobre depSobre) {
+        super();
+        this.depSobre = depSobre;
+        ubicacion = new Point(x, y);
+        setBounds(x,y,size.width, size.height);
+        new Hitbox(ubicacion.x ,ubicacion.y,size.width - 60,size.height,0);
+    }
+
+    public DepSobre getDepSobre() {
+        return depSobre;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.setColor(new Color(100, 100, 200));
+        g.fillRect(ubicacion.x, ubicacion.y, 60, 100);
+    }
+}
