60da9f4966c125b7ce3179dc65bfacd6b694b869
Rhussu <wzaratesolar@gmail.com>
2023-11-23 23:33:48 -0300

    Solo en desarrollo el puente comenzará reparado

--- Programa/src/main/java/Interfaz/PanelPuente.java
+++ Programa/src/main/java/Interfaz/PanelPuente.java
@@ -11,14 +11,20 @@ public class PanelPuente extends JPanel {
     private static Dimension size = new Dimension(250, 200);
     private static Point ubicacion;
     private static Puente puente = new Puente();
+    private Hitbox hitbox;
     public PanelPuente(int x, int y) {
         ubicacion = new Point(x, y);
         setBounds(x,y,size.width, size.height);
-        new Hitbox(ubicacion.x + 60,ubicacion.y,size.width - 120,size.height,0);
+        hitbox = new Hitbox(ubicacion.x + 60,ubicacion.y,size.width - 120,size.height,3);
+        puente.setFuncional(true);
     }
 
     public static Puente getPuente() {
         return puente;
+    }
+
+    public Hitbox getHitbox() {
+        return hitbox;
     }
 
     public static Point getUbicacion() {
