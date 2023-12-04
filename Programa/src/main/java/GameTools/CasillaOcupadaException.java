f073d807a1a39f88d617c2ef4285675317d67f64
Angie161 <146099263+Angie161@users.noreply.github.com>
2023-12-04 15:57:07 -0300

    Agregado de documentación en CasillaOcupadaException

--- Programa/src/main/java/GameTools/CasillaOcupadaException.java
+++ Programa/src/main/java/GameTools/CasillaOcupadaException.java
@@ -1,7 +1,13 @@
 package GameTools;
 
+/**
+ * Excepción arrojada cuando se intenta colocar una nueva hitbox encima de un espacio que ya se encuentra ocupado por otra hitbox.
+ */
 public class CasillaOcupadaException extends Exception {
     public CasillaOcupadaException() {
+     /**
+     * Constructor por defecto.
+     */
         super();
     }
 }
