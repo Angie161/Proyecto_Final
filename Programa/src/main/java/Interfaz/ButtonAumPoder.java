fdeef19c0a7c7737b5e41d1ead4d21905095d09c
Angie161 <146099263+Angie161@users.noreply.github.com>
2023-12-04 16:42:42 -0300

    Agregada la documentación de ButtonAumPoder

--- Programa/src/main/java/Interfaz/ButtonAumPoder.java
+++ Programa/src/main/java/Interfaz/ButtonAumPoder.java
@@ -7,7 +7,16 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+/**
+ * Clase que genera un botón de "Aumento de poder" en el panel menú del Mausoleo, el cual al presionarlo permite aumentar el poder de la muerte.
+ */
 public class ButtonAumPoder extends JButton {
+
+    /**
+     * Constructor de ButtonAumCap, configura los parámetros iniciales para que se muestren en pantalla.
+     *
+     * @param panelMapa con el cuál se accede a Mausoleo para modificar el Poder.
+     */
     public ButtonAumPoder(PanelMapa panelMapa){
         super();
         setBounds(600,33,350,96);
@@ -15,9 +24,17 @@ public class ButtonAumPoder extends JButton {
         setBorder(null);
 
         addMouseListener(new MouseListener() {
+            /**
+             * Método no empleado.
+             */
             @Override
             public void mouseClicked(MouseEvent e) {}
 
+            /**
+             * Evento ejecutado al presionar el botón. Genera un aumento de poder si se tiene la cantidad de dinero requerida.
+             *
+             * @param e evento a ser procesado.
+             */
             @Override
             public void mousePressed(MouseEvent e) {
                 try {
@@ -28,14 +45,27 @@ public class ButtonAumPoder extends JButton {
                 }
             }
 
+            /**
+             * Método no empleado.
+             */
             @Override
             public void mouseReleased(MouseEvent e) {}
 
+            /**
+             * Evento ejecutado al entrar al botón. Oscurece el fondo del botón para generar el efecto de activación.
+             *
+             * @param e evento a ser procesado.
+             */
             @Override
             public void mouseEntered(MouseEvent e) {
                 setBackground(new Color(0,0,0,100));
             }
 
+            /**
+             * Evento ejecutado al salir del botón. Vuelve el botón a su color original.
+             *
+             * @param e evento a ser procesado.
+             */
             @Override
             public void mouseExited(MouseEvent e) {
                 setBackground(new Color(0,0,0,0));
