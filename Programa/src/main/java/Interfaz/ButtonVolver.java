23f50c77e1ed72792b0c95b733be5799e5b8c100
Angie161 <146099263+Angie161@users.noreply.github.com>
2023-12-04 17:19:02 -0300

    Añadida la documentación de ButtonVolver

--- Programa/src/main/java/Interfaz/ButtonVolver.java
+++ Programa/src/main/java/Interfaz/ButtonVolver.java
@@ -5,7 +5,16 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+/**
+ * Clase que genera un botón para salir del panel menú del Mausoleo.
+ */
 public class ButtonVolver extends JButton {
+    
+    /**
+     * Constructor de ButtonVolver, configura los parámetros iniciales para que se muestren en pantalla.
+     *
+     * @param panelMenuMausoleo con el cual se configura la visibilidad de este.
+     */
     public ButtonVolver(JPanel panelMenuMausoleo){
         super();
         setBorder(null);
@@ -13,23 +22,44 @@ public class ButtonVolver extends JButton {
         setBackground(new Color(0,0,0,0));
 
         addMouseListener(new MouseListener() {
+            /**
+             * Método no empleado.
+             */
             @Override
             public void mouseClicked(MouseEvent e) {}
 
+            /**
+             * Evento ejecutado al presionar el botón. Convierte la visibilidad del MenuMausoleo.
+             *
+             * @param e evento a ser procesado.
+             */
             @Override
             public void mousePressed(MouseEvent e) {
                 panelMenuMausoleo.setVisible(false);
             }
 
+            /**
+             * Método no empleado.
+             */
             @Override
             public void mouseReleased(MouseEvent e) {
             }
 
+            /**
+             * Evento ejecutado al entrar al botón. Oscurece el fondo del botón para generar el efecto de activación.
+             *
+             * @param e evento a ser procesado.
+             */
             @Override
             public void mouseEntered(MouseEvent e) {
                 setBackground(new Color(0,0,0,100));
             }
 
+            /**
+             * Evento ejecutado al salir del botón. Vuelve el botón a su color original.
+             *
+             * @param e evento a ser procesado.
+             */
             @Override
             public void mouseExited(MouseEvent e) {
                 setBackground(new Color(0,0,0,0));
