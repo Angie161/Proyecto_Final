ce9542d741030f746135a7ab24caa1d5171b06f8
Angie161 <aramirez2022@udec.cl>
2023-12-07 00:15:03 -0300

    Agregada la documentación de los paneles de los depósitos

--- Programa/src/main/java/Interfaz/PanelSalidaDepSobre.java
+++ Programa/src/main/java/Interfaz/PanelSalidaDepSobre.java
@@ -6,11 +6,23 @@ import Logica.*;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Clase que representa dentro de la interfaz a los depósitos sobrenaturales que almacenan almas que se encuentran en el cielo, y con los cuales se podrá sacar dichas almas.
+ * Solo sirven para obtener las almas almacenadas, no para guardarlas.
+ */
 public class PanelSalidaDepSobre extends JPanel {
     private static Dimension size = new Dimension(120, 130);
     private Point ubicacion;
     private DepSobre depSobre;
     private ImageIcon imageDepSobre;
+
+    /**
+     * Constructor del PanelSalidaDepSobre con el cual se establecen los parámetros iniciales de visualización dentro del mapa.
+     * @param x Establece la ubicación horizontal del cofre en la pantalla.
+     * @param y Establece la ubicación vertical del cofre en la pantalla.
+     * @param depSobre al cual está asociado.
+     * @param rutaASuImagen, aspecto que tendrá el cofre.
+     */
     public PanelSalidaDepSobre(int x, int y, DepSobre depSobre, String rutaASuImagen) {
         super();
         this.depSobre = depSobre;
@@ -25,10 +37,19 @@ public class PanelSalidaDepSobre extends JPanel {
         }
     }
 
+    /**
+     * Getter del DepSobre al cual se encuentra asociado el panel-
+     * @return DepSobre.
+     */
     public DepSobre getDepSobre() {
         return depSobre;
     }
 
+    /**
+     * Override del método paintComponent(g) para dibujar personalizadamente.
+     *
+     * @param g the <code>Graphics</code> object to protect
+     */
     @Override
     protected void paintComponent(Graphics g) {
         try {
