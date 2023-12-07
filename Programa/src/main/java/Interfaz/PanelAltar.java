b5339d501ae9d01cf9b2669246b3328e0c683560
Angie161 <aramirez2022@udec.cl>
2023-12-07 00:16:02 -0300

    Añadida la documentación de las clases faltantes dentro de Interfaz

--- Programa/src/main/java/Interfaz/PanelAltar.java
+++ Programa/src/main/java/Interfaz/PanelAltar.java
@@ -7,9 +7,19 @@ import Logica.*;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Representa visualmente los altares que se encuentran en el cielo, con los que se podrán fusionar almas.
+ */
 public class PanelAltar extends JPanel {
     private PanelAlma panelAlma;
     private ImageIcon imageAltar;
+
+    /**
+     * Contructor de PanelAltar, el cuál establecerá su ubicación en la pantalla y su imagen asociada.
+     * @param x Componente horizontal donde se dibujará.
+     * @param y Componente vertical donde se dibujará.
+     * @param rutaASuImagen
+     */
     public PanelAltar(int x, int y, String rutaASuImagen) {
         super();
         setOpaque(false);
@@ -24,16 +34,29 @@ public class PanelAltar extends JPanel {
         }
     }
 
+    /**
+     * Getter del panelAlma el cuál contendrá una vez el usuario coloque un alma sobre el altar.
+     * @return PanelAlma solicitado.
+     */
     public PanelAlma getPanelAlma() {
         return panelAlma;
     }
 
+    /**
+     * Elimina el alma del mapa al momento de fusionar dos almas.
+     * @param panelMapa donde se encuentra el altar y con las que se podrá acceder a los paneles de las almas.
+     * @param cual para saber en cuál de sus altares se encuentra el alma a quitar.
+     */
     public void quitarAlma(PanelMapa panelMapa, int cual) {
         panelMapa.remove(panelMapa.getPanelAltar()[cual].getPanelAlma());
         Hitbox.eliminarHitbox(panelMapa.getPanelAltar()[cual].getPanelAlma().getHitbox());
         panelAlma = null;
     }
 
+    /**
+     * Hace que el panel del alma se establezca al centro del altar.
+     * @param h la hitbox asociada al panel del alma que se introduce.
+     */
     public void ingresarAlma(Hitbox h) {
         if(this.getBounds().contains(h.getHitbox()) && panelAlma == null && !Hitbox.getTodasLasHitbox().get(0).getHitbox().intersects(this.getBounds())) {
             h.setLocation(getLocation().x + (getSize().width - 110),getLocation().y + (getSize().height - h.getPanelAsociado().getSize().height)/2 - 25);
@@ -42,6 +65,11 @@ public class PanelAltar extends JPanel {
             panelAlma = null;
         }
     }
+
+    /**
+     * Fusiona las almas que se encuentran en los altares de fusión.
+     * @param panelMapa con el cual se accede a los paneles que se encuentran en él.
+     */
     public void fucionarAlmas(PanelMapa panelMapa) {
         Almas almas1 = panelMapa.getPanelAltar()[0].getPanelAlma().getAlma();
         Almas almas2 = panelMapa.getPanelAltar()[1].getPanelAlma().getAlma();
@@ -56,6 +84,12 @@ public class PanelAltar extends JPanel {
             panelMapa.getPanelAltar()[1].quitarAlma(panelMapa, 1);
         }
     }
+
+    /**
+     * Override del método paintComponent(g) para dibujar personalizadamente.
+     *
+     * @param g the <code>Graphics</code> object to protect
+     */
     @Override
     protected void paintComponent(Graphics g) {
         try {
--- Programa/src/main/java/Interfaz/PanelPuente.java
+++ Programa/src/main/java/Interfaz/PanelPuente.java
@@ -7,12 +7,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
+/**
+ * Representa un puente en la interfaz, y con el cual se podrá acceder al cielo.
+ */
 public class PanelPuente extends JPanel {
     private static Dimension size = new Dimension(260, 220);
     private static Point ubicacion;
     private static Puente puente = new Puente();
     private Hitbox hitbox;
     private ImageIcon[] imagePuente = new ImageIcon[2];
+
+    /**
+     * Constructor de PanelPuente, que añade su hitbox y establece las imágenes asociadas a este.
+     * @param x Establece la ubicación horizontal del cofre en la pantalla.
+     * @param y Establece la ubicación vertical del cofre en la pantalla.
+     */
     public PanelPuente(int x, int y) {
         ubicacion = new Point(x, y);
         setBounds(x,y,size.width, size.height);
@@ -21,21 +30,41 @@ public class PanelPuente extends JPanel {
         imagePuente[1] = new ImageIcon(getClass().getClassLoader().getResource("Imagenes/Mapa/PuenteNotOk.png"));
     }
 
+    /**
+     * Getter del puente que está asociado al panelPuente.
+     * @return Puente.
+     */
     public static Puente getPuente() {
         return puente;
     }
 
+    /**
+     * Getter de la hitbox asociada al panelPuente.
+     * @return
+     */
     public Hitbox getHitbox() {
         return hitbox;
     }
 
+    /**
+     * Getter de la ubicación del puente dentro de la interfaz.
+     * @return Point con la ubicación.
+     */
     public static Point getUbicacion() {
         return ubicacion;
     }
 
+    /**
+     * Getter del tamaño del puente.
+     * @return Dimension.
+     */
     public static Dimension getPuenteSize() {
         return size;
     }
+
+    /**
+     * Actualiza el estado de la hitbox del puente, es decir, si el jugador puede estar encima de este cuando se encuentra reparado o si, por el contrario, no puede pasar al estar roto.
+     */
     public void acutalizarHitbox() {
         if(puente.getFuncional()) {
             hitbox.setTraspasableForPlayer(true);
@@ -44,6 +73,11 @@ public class PanelPuente extends JPanel {
         }
     }
 
+    /**
+     * Override del método paintComponent(g) para dibujar personalizadamente.
+     *
+     * @param g the <code>Graphics</code> object to protect
+     */
     @Override
     protected void paintComponent(Graphics g) {
         acutalizarHitbox();
